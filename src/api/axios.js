c2cc35f6d8b4b5f429f38ad0c6ecf5a01bb7417b
Mohamad Rabbani <mrabbani@outlook.com>
2024-05-26 09:20:15 +0500

    server url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,18 +1,24 @@
 import axios from "axios";
 
 let BASE_URL;
-// BASE_URL = "https://todoapp-server-hj1x.onrender.com";
-BASE_URL = "https://todo-app-server-mohamad-rabanis-projects.vercel.app/";
+BASE_URL = "https://todoapp-server-hj1x.onrender.com";
+// BASE_URL = "https://todo-app-server-mohamad-rabanis-projects.vercel.app/";
 // BASE_URL = "http://localhost:3000";
 
 export default axios.create({
   baseURL: BASE_URL,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
   withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
   withCredentials: true,
 });
